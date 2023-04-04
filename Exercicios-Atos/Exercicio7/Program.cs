fbf2cc5683ad07fc0677bf6389465a2e25fe4994
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2023-04-04 16:02:35 -0300

    exercicios e organização de pastas

---
+++ Exercicios-Atos/Exercicio7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio7
+{
+    internal class Program
+    {
+        //Escreva um programa em C# e no Visual Studio que leia o número de um vendedor, o seu salário fixo,o total de vendas por ele efetuadas e o percentual que ganha sobre o total de vendas. Calcule o salário total do vendedor.Escreva o número do vendedor e o salário total do vendedor.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Número do vendedor:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o Salário:");
+            double salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o total de vendas realizadas:");
+            int totalVendas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o percentual sobre o total de vendas :");
+            double percentual= double.Parse(Console.ReadLine());
+            double calculoPercentual= (percentual/100)*totalVendas;
+            Console.WriteLine($"Número do vendedor : {num} - Salário total : {salario + calculoPercentual}");
+        }
+    }
+}
---
+++ Exercicios-Atos/Exercicio8/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit A fórmula da conversão é F = (9 * C + 160) / 5.
+            Console.WriteLine("Conversor de temperatura Celsius - Fahrenheit");
+            Console.WriteLine("insira a temperatura em celsius: ");
+            double x = double.Parse(Console.ReadLine());
+
+            double toFahrenheit = (x * 9 / 5) + 32;
+            Console.WriteLine("A temperatura em fahrenheit é " + toFahrenheit);
+        }
+    }
+}
