e1bb3e7a01845f386dceae3f49c41446e0666a55
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2023-04-04 17:08:57 -0300

    exercicios

---
+++ Exercicios-Atos/Exercicio-Calculadora/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercicio_Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculadora Simples com escolha da operação pro parte do usuário
+            Console.WriteLine("CALCULADORA");
+            Console.WriteLine("Insira o primeiro número");
+            double numA = double.Parse(Console.ReadLine());  
+            Console.WriteLine("Insira o segundo número");
+            double numB = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha uma operação: 1 - SOMA; 2 - SUBTRAÇÃO; 3 - DIVISÃO; 4 - MULTIPLICAÇÃO");
+            int operation= int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case 1:
+                    Console.WriteLine($"A soma é {numA + numB }");
+                    break;
+                case 2:
+                    Console.WriteLine($"A subtração é {numA - numB}");
+                    break;
+                case 3:
+                    double result = numA / numB;
+                    Console.WriteLine($"A divisão é {result}");
+                    break;
+                case 4:
+                    Console.WriteLine($"A multiplicação é {numA * numB}");
+                    break;
+               
+            }
+        }
+    }
+}
